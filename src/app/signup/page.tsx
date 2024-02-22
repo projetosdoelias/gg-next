40e57bc4aca761869816afe8cbf508a0b4cce414
Elias Lourenço <elias.oliveira@novaescola.org.br>
2024-02-21 23:42:01 -0300

    add tela cadastro

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,60 @@
+'use client'
+import { createUser } from '../actions'
+
+export default function Signup() {
+
+  return (
+    <div  className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
+        <h1 className="text-3xl font-bold text-center text-gray-700">Logo</h1>
+        <form action={createUser} className="mt-6">
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Password
+            </label>
+            <input
+              name='password'
+              type="password"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+
+          <div className="mb-2">
+            <label
+              htmlFor="password_confirm"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Confirm Password
+            </label>
+            <input
+              name='password_confirm'
+              type="password"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mt-2 pt-[15px]">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
