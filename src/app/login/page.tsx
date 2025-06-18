bb599961c80ea16cb924d9c682898fad2fe16d6a
projetosdoelias <projetosdoelias@gmail.com>
2025-06-17 21:59:49 -0300

    new infrastructure

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,123 +1,103 @@
-'use client'
-import 'react-toastify/dist/ReactToastify.css';
-import { useState } from 'react'
-import { showErrorToast, showSuccessToast } from '@/src/common/utils/toast-message.helper';
-import { useRouter } from 'next/navigation'
-import { cookies } from 'next/headers'
-
+import Image from "next/image";
 
 export default function Login() {
-
-  const router = useRouter()
-  const [user, setUser] = useState({ email: '', password: '', password_confirm: '', name: '' });
-  const [submitDisabled, setSubmitDisabled] = useState(false);
-  const handleInputChange = (e: any) => {
-
-    const { name, value } = e.target;
-    setUser((prevFormulario: any) => ({
-      ...prevFormulario,
-      [name]: value,
-    }));
-  };
-
-  const submitLoginForm = async (event: any) => {
-
-    event.preventDefault();
-    const defaultErrorMsg = 'Oops! Something went wrong during sign-up. Please try again later.'
-
-    const data = {
-      email: event.target.email.value,
-      password: event.target.password.value,
-    };
-
-    const retorno = await fetch(`/api/session`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data),
-
-    })
-
-
-    if (!retorno) {
-      showErrorToast(defaultErrorMsg, 3000)
-      return;
-    }
-
-    const dataReturn = await retorno.json()
-    try {
-
-      if ((retorno?.status === 201 || retorno?.status === 200)) {
-        showSuccessToast("Welcome back!");
-        return;
-      } else {
-        const errorMessage = dataReturn?.message ?? defaultErrorMsg;
-        throw new Error(errorMessage);
-      }
-
-    } catch (error: any) {
-      showErrorToast(error?.message, 3000)
-      return;
-    }
-  };
-
   return (
-    <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
 
-      <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
-        <h1 className="text-3xl font-bold text-center text-gray-700">Logo</h1>
-        <form
-          onSubmit={submitLoginForm}
-          className="mt-6">
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold text-gray-800"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              required
-              name="email"
-              value={user.email}
-              onChange={handleInputChange}
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
             />
-          </div>
-          <div className="mb-2">
-            <label
-              htmlFor="password"
-              className="block text-sm font-semibold text-gray-800"
-            >
-              Password
-            </label>
-            <input
-              required
-              name='password'
-              type="password"
-              value={user.password}
-              onChange={handleInputChange}
-
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div>
-          <div className="mt-2 pt-[15px]">
-            <button
-              type="submit"
-              disabled={submitDisabled}
-              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-              Login
-            </button>
-          </div>
-          <div className='content-center text-center'>
-            <p className=" pt-[10px] text-[14px] ">Not a member yet? <a onClick={() => { router.push('/signup') }} className='font-bold cursor-pointer' >Sign up and lets grow!</a></p>
-          </div>
-        </form>
-      </div>
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
-
-  )
+  );
 }
